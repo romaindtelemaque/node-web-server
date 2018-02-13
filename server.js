3654a7cb29fa363929c28276fa49f261d61102f3
romain drevet <romaindrevet.dev@gmail.com>
2018-02-13 09:12:20 +0100

    correct port var

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3200;
 
 var app = express();
 
@@ -71,5 +71,5 @@ app.get('/bad', (request, response) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is up on port ${pos}`);
+  console.log(`Server is up on port ${port}`);
 });
